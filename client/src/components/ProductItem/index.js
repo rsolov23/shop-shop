aae5e56e52f16d336c7912fecfdfd69e08867848
Rachel Solov <rsolov23@yahoo.com>
2021-08-11 12:09:46 -0500

    finished lesson 3

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { idbPromise } from "../../utils/helpers";
 import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
@@ -16,11 +17,16 @@ function ProductItem(item) {
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
       });
+      idbPromise("cart", "put", {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 },
       });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
     }
   };
   return (
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { idbPromise } from "../../utils/helpers";
 import { useQuery } from "@apollo/client";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
@@ -16,8 +17,21 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+      // but let's also take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise("products", "put", product);
+      });
+    } else if (!loading) {
+      //since we are offline, get all data from products store
+      idbPromise("products", "get").then((products) => {
+        //use retieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
